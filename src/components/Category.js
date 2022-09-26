dc5d6d67b962e0238acad0ee7b1442a0999d8eb1
Calebbii <biicaleb316@gmail.com>
2022-09-26 05:57:20 +0300

    Category section updated

---
+++ src/components/Category.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style/category.css';
+
+export default function Category({meals}) {
+  return (
+    <div id='container'>
+      <div className='category' >
+        <img className="categoryImg" src={meals.strMealThumb}/>
+        <h1 className='categoryName'style={{ color : "darkblue"}}>{meals.strMeal}</h1>
+      </div>
+    </div>
+    
+  )
+}
